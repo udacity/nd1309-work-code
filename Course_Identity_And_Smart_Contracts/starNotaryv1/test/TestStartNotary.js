8e6ae6a2dc072b04c0dbe64eb92c6ba32343e589
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-24 12:42:00 -0500

    Updating the v1 Test file

--- Course_Identity_And_Smart_Contracts/starNotaryv1/test/TestStartNotary.js
+++ Course_Identity_And_Smart_Contracts/starNotaryv1/test/TestStartNotary.js
@@ -1,49 +1,40 @@
-
+// Importing the StartNotary Smart Contract ABI (JSON representation of the Smart Contract)
 const StarNotary = artifacts.require("StarNotary");
 
-var accounts;
-var owner;
+var accounts; // List of development accounts provided by Truffle
+var owner; // Global variable use it in the tests cases
 
+// This called the StartNotary Smart contract and initialize it
 contract('StarNotary', (accs) => {
-    accounts = accs;
-    owner = accounts[0];
+    accounts = accs; // Assigning test accounts
+    owner = accounts[0]; // Assigning the owner test account
 });
 
-it('has correct name', () => {
-    StarNotary.deployed().then((instance) => {
-        return instance.starName.call();
-    }).then((starName) => {
-        assert.equal(starName, "Awesome Udacity Star");
-    })
+// Example test case, it will test if the contract is able to return the starName property 
+// initialized in the contract constructor
+it('has correct name', async () => {
+    let instance = await StarNotary.deployed(); // Making sure the Smart Contract is deployed and getting the instance.
+    let starName = await instance.starName.call(); // Calling the starName property
+    assert.equal(starName, "Awesome Udacity Star"); // Assert if the starName property was initialized correctly
 });
 
-it('can be claimed', () => {
-    let instance;
-    StarNotary.deployed().then((instanceAux) => {
-        instance = instanceAux;
-        return instance.claimStar({from: owner});
-    }).then((result) => {
-        return instance.starOwner.call();
-    }).then((starOwner) => {
-        assert.equal(starOwner, owner)
-    })
+// Example test case, it will test is the Smart Contract function claimStar assigned the Star to the owner address
+it('can be claimed', async () => {
+    let instance = await StarNotary.deployed(); // Making sure the Smart Contract is deployed and getting the instance.
+    await instance.claimStar({from: owner}); // Calling the Smart Contract function claimStar
+    let starOwner = await instance.starOwner.call(); // Getting the owner address
+    assert.equal(starOwner, owner); // Verifying if the owner address match with owner of the address
 });
 
+// Example test case, it will test is the Smart Contract function claimStar assigned the Star to the owner address and it can be changed
 it('can change owners', async () => {
-    let instance;
+    let instance = await StarNotary.deployed();
     let secondUser = accounts[1];
-    StarNotary.deployed().then((instanceAux) => {
-        instance = instanceAux;
-        return instance.claimStar({from: owner})
-    }).then((result) => {
-        return instance.starOwner.call();
-    }).then((starOwner) => {
-        assert.equal(starOwner, owner);
-        return instance.claimStar({from: secondUser});
-    }).then((resultSec) => {
-        return instance.starOwner.call();
-    }).then((starOwnerSecond) => {
-        assert.equal(starOwnerSecond, secondUser);
-    })
- })
+    await instance.claimStar({from: owner});
+    let starOwner = await instance.starOwner.call();
+    assert.equal(starOwner, owner);
+    await instance.claimStar({from: secondUser});
+    let secondOwner = await instance.starOwner.call();
+    assert.equal(secondOwner, secondUser);
+ });
 
