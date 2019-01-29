18a23156318019e0ba2254d83f7a45566b2f4283
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-29 14:50:56 -0500

    Updated with the latest version of Solidity

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/arraysContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/arraysContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.24;
 
 contract ArraysContract {
 
@@ -10,28 +10,20 @@ contract ArraysContract {
     // Dynamic array of type bool
     bool[]     dynamicBoolArray;
 
-
     function testArray() public {
-
         // Allocate memory for 8 elements to the dynamic bool storage array
         dynamicBoolArray = new bool[](8);
-
         // Allocate memory and initialize elements in the int array
         // Explicit conversion is needed from uint8 to int8
         dynamicIntArray = [int8(1),2,3];
-
         // This will work fine, since we are inside a function
         uint8[] memory memoryArray; // A Dynamic memory array
-
         // Allocation with assignment not allowed for dynamic memory arrays
         // memoryArray = [1,2,3];
-
-         uint8[] memory dynamicMemoryArray;  // A Dynamic memory array
+        uint8[] memory dynamicMemoryArray;  // A Dynamic memory array
         // Assignment of dynamic NOT allowed
         //dynamicMemoryArray = [uint8(1),2];
-
         memoryArray = new uint8[](8);
-
         // push() not allowed for memory array
         // memoryArray.push(1);
         // memoryArray.length=6;
@@ -41,7 +33,6 @@ contract ArraysContract {
         // Static storage array assignment is OK
         // Compile time size check is carried out
         // so assignment to [1,2,3,4] will fail below
-
         staticIntArray = [1,2,3];
 
 
