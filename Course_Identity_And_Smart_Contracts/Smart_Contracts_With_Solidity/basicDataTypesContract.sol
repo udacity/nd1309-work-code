6bd7b371b9844947a16ba81f460b0f8650e8618f
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-29 14:41:01 -0500

    Update with Solidity last Version

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/basicDataTypesContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/basicDataTypesContract.sol
@@ -1,25 +1,19 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.24;
 
-contract BasicDataTypesContract {
+contract BasicDataType {
 
-    uint8   a = 255;   // 1 bytes unsigned integer
-
-    address public owner;      // Address types variable, called owner
-
+    uint8   a = 255;             // 1 bytes unsigned integer
+    address public owner;        // Address types variable, called owner
     bool  public flag = true;
-
     uint    ownerInitialBalance; // uint type variable
 
-// Function takes in an address, and returns the balance of this address
+    // Function takes in an address, and returns the balance of this address
     function test (address addr) public returns (uint) {
-
         owner = addr;
-
         ownerInitialBalance = owner.balance;
-
         // if(1){ } will not compile
-
-        if (1 > 0) {  // This will work because expression evaluates to bool
+        if (1 > 0) {
+            // This will work because expression evaluates to bool
             // Do something
         }
         return ownerInitialBalance;
