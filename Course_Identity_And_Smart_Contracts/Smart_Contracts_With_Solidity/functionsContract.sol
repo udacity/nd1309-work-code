0e9228d98c6ad5c65f53f0c442b5c7b112c337df
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-30 08:35:41 -0500

    Updated to the latest version of Solidity

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/functionsContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/functionsContract.sol
@@ -1,40 +1,40 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.24;
 
-contract Funcs {
+contract FunctionsContract {
 
-  string  ownerName;
-  uint8   ownerAge;
+    string ownerName;
+    uint8  ownerAge;
 
-  // Constructor
-  constructor (string name, uint8 age) public {
-    ownerName = name;
-    ownerAge = age;
-  }
+    // Constructor
+    constructor (string memory name, uint8 age) public {
+        ownerName = name;
+        ownerAge = age;
+    }
 
-  // We are changing the owner name and age
-  function  setOwnerInfo(string name, uint8 age) public {
-    ownerName = name;
-    ownerAge = age;
-  }
+    // We are changing the owner name and age
+    function setOwnerInfo(string memory name, uint8 age) public {
+        ownerName = name;
+        ownerAge = age;
+    }
 
-  function secretFunction() private pure {
-    // Not available outside this contract
-  }
-// Get owner name and age
-  function  getOwnerInfo() public view returns (string name, uint8 age){
-    name = ownerName;
-    age = ownerAge;
-  }
+    function secretFunction() private pure {
+        // Not available outside this contract
+    }
 
-// Get the name
-// 2 ways to return values from a function
-  function  getOwnerName() public view returns (string) {
-    return ownerName;
-  }
+    // Get owner name and age
+    function  getOwnerInfo() public view returns (string memory name, uint8 age){
+        name = ownerName;
+        age = ownerAge;
+    }
 
-  // Get the age
-  function  getOwnerAge() public view returns(uint8 age){
-    age = ownerAge;
-    //return ownerAge;
-  }
+    // Get the name
+    // 2 ways to return values from a function
+    function getOwnerName() public view returns (string memory) {
+        return ownerName;
+    }
+
+    // Get the age
+    function getOwnerAge() public view returns (uint8 age){
+        age = ownerAge;
+    }
 }
