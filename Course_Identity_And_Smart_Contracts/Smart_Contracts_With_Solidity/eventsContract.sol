b12a949e539768ffb97c9c7a444e23b03a2bd07c
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-30 08:55:40 -0500

    Updated to the latest version of Solidity

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/eventsContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/eventsContract.sol
@@ -1,21 +1,20 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.4.24;
 
 contract EventsContract {
-
     // Represents the time when the bidding will end
-    uint    biddingEnds = now + 5 days;
+    uint biddingEnds = now + 5 days;
 
     struct HighBidder {
-        address   bidder;
-        string    bidderName;
-        uint      bid;
+        address bidder;
+        string bidderName;
+        uint bid;
     }
-// Variable of type HighBidder
-    HighBidder  public highBidder;
+
+    // Variable of type HighBidder
+    HighBidder public highBidder;
 
     // Events emitted by contract
     // Whenever a high bid is received
-
     event NewHighBid(address indexed who, string name, uint howmuch);
 
     // High bid preceded by this event
@@ -23,39 +22,34 @@ contract EventsContract {
 
     // Ensures that bid can be received i.e, auction not ended
     modifier timed {
-        if(now < biddingEnds) {
+        if(now < biddingEnds){
             _;
         } else {
             /**throw an exception */
-            revert();
+            revert("Throw an exception");
         }
     }
 
-    constructor() public{
+    constructor() public {
         // Starts the bidding at 1 ether
         highBidder.bid = 1 ether;
     }
-// Payable since ether should be coming along
-// Timed, we need to end this bidding in 5 days
-    function  bid(string bidderName) payable public timed {
 
+    
+    // Payable since ether should be coming along
+    // Timed, we need to end this bidding in 5 days
+    function bid(string memory bidderName) public payable timed {
         if(msg.value > highBidder.bid) {
-
             highBidder.bidder = msg.sender;
             highBidder.bidderName = bidderName;
             highBidder.bid = msg.value;
-
             // Received a high bid - emit event
             emit NewHighBid(msg.sender, bidderName, msg.value);
-          }
-
-          else {
-
+        } else {
             // Received bid less than high bid emit event
             emit BidFailed(msg.sender, bidderName, msg.value);
-
             // throwing exception would return the ethers
-            revert();
+            revert("Throw an exception");
         }
     }
 }
