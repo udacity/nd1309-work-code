40ded391113ea4797d5ad38c225d208b9d2c99f3
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-30 08:16:34 -0500

    Updated to the latest version of Solidity

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/structsContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/structsContract.sol
@@ -1,24 +1,22 @@
-pragma solidity ^0.4.4;
+pragma solidity >=0.4.24;
 
 contract StructsContract {
 
-
-    // Country
-    struct family {
+    // Family
+    struct Family {
         bytes32 lastName;
         uint8 houseNo;
-        uint16  age;
+        uint16 age;
     }
 
     // Creating an array of type family..which is a struct
-    family []  myFamily;
+    Family[] myFamily;
 
     // Getting a lastName, and returning complete
     // family details
     // We cannot compare 2 strings in solidity...
     function getName(bytes32 name) public view returns (bytes32, uint8, uint16) {
         // Search the array
-
         for(uint8 i = 0; i < myFamily.length; i++){
             if(name == myFamily[i].lastName) {
                 return (myFamily[i].lastName,uint8(myFamily[i].houseNo), myFamily[i].age);
@@ -28,17 +26,14 @@ contract StructsContract {
 
     // Structs Cannot be passed as argument so we are passing all elements/attributes of struct as args
     function addName(bytes32 _lastName, uint8 _value, uint16 _age) public returns (bool) {
-
         // Declare the struct variable in memory...
-        family memory newFamily;
-
+        Family memory newFamily;
         //  use the . notation to access members of a struct
         newFamily.lastName = _lastName;
         newFamily.houseNo = _value;
         newFamily.age = _age;
-
         // Push the newFamily struct...into our myFamily array
-       myFamily.push(newFamily);
+        myFamily.push(newFamily);
         return true;
     }
 }
