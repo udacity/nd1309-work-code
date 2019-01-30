0d0e48a069244f9fe6170eec0c352c1439cb77c3
Jose Perera Morales <jose.morales@Joses-MBP.fios-router.home>
2019-01-30 08:39:19 -0500

    Updated to the latest version of Solidity

--- Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/exceptionsContract.sol
+++ Course_Identity_And_Smart_Contracts/Smart_Contracts_With_Solidity/exceptionsContract.sol
@@ -1,25 +1,25 @@
-pragma solidity ^0.4.4;
+pragma solidity >=0.4.24;
 
-contract exceptionContract {
+contract ExceptionsContract {
 
     // Public variable lastCaller, with a value of none
     string  public lastCaller = "none";
 
     // Function revertBehavior that takes in a name as an argument,
     //and sets the lastCaller variable to this argument received
-    function  revertBehavior(string name) public returns (bool) {
+    function  revertBehavior(string memory name) public returns (bool) {
         lastCaller = name;
 
         // Check if length of the string is zero
         // If the argument received has zero length, it throws an exception...
         // Once an exception is thrown, the variable lastCaller rolls back to its initial value
         if (bytes(name).length == 0) {
-            revert();
+            revert("The length of the string is zero");
         }
         // This is the same thing...just using require to check the length of the input string.
         // The code will only be exceuted if the length is greater than 0
         // The above lines of code may be replaced with this
-        require(bytes(name).length > 0);
+        require(bytes(name).length > 0, "The length of the string is zero");
 
         return true;
     }
