34e33e6a6ba52bc7b91c37ec720b90e6c38a6393
Noah Figueras <33466101+noahfigueras@users.noreply.github.com>
2020-11-10 19:57:03 -0800

    Update StarNotary.sol

--- Course_Identity_And_Smart_Contracts/starNotaryv2/contracts/StarNotary.sol
+++ Course_Identity_And_Smart_Contracts/starNotaryv2/contracts/StarNotary.sol
@@ -37,7 +37,7 @@ contract StarNotary is ERC721 {
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
         require(msg.value > starCost, "You need to have enough Ether");
-        _transferFrom(ownerAddress, msg.sender, _tokenId);
+        _transfer(ownerAddress, msg.sender, _tokenId);
         address payable ownerAddressPayable = _make_payable(ownerAddress);
         ownerAddressPayable.transfer(starCost);
         if(msg.value > starCost) {
