bd30a5e4cfeb99b7f4e295f9661cdd8823681eee
cfav <chrisfavila.09@gmail.com>
2023-01-03 09:12:59 +0800

    Add Goerli settings in SampleToken truffle config

--- Course_Identity_And_Smart_Contracts/SampleToken/truffle-config.js
+++ Course_Identity_And_Smart_Contracts/SampleToken/truffle-config.js
@@ -37,11 +37,10 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
         network_id: 4,       // rinkeby's id
-        gas: 4500000,        // rinkeby has a lower block limit than mainnet
-        gasPrice: 10000000000
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+        network_id: 5,       // goerli's id
     },
   },
 
